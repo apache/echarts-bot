62373008af7f98f7099468a69b975d977e932e2d
plainheart <yhen@all-my-life.cn>
2021-11-29 21:07:08 +0800

    not comment anymore when an issue was opened

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = (/** @type import('probot').Probot */ app) => {
 
         await issue.init();
 
-        await commentIssue(context, text.ISSUE_CREATED);
+        // await commentIssue(context, text.ISSUE_CREATED);
 
         const addLabels = issue.addLabels.length
             && context.octokit.issues.addLabels(
