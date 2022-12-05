6fc5e8b256285354966860342ed275dea4323cea
plainheart <yhen@all-my-life.cn>
2022-12-05 17:10:11 +0800

    use node-fetch to wake up

--- src/glitch/wakeup.js
+++ src/glitch/wakeup.js
@@ -1,12 +1,12 @@
-const got = require('got');
+const fetch = require('node-fetch');
 
 const WAKEUP_URL = 'https://apache-incubator-echarts-bot-1.glitch.me/probot';
 
 (async function() {
-  const { body } = await got(WAKEUP_URL, {
+  const { body } = await fetch(WAKEUP_URL, {
     headers: {
       'Accept': 'text/html',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
   });
   if (body.indexOf('Welcome to') === -1) {
